1ea116c9678925caecea564b1b39cbc4b4006060
first8411 <92551338+first8411@users.noreply.github.com>
2022-02-15 18:12:46 -0600

    :tada: Initial Commit

--- index.ts
+++ index.ts
@@ -3,13 +3,42 @@ import fastify from "fastify";
 const server = fastify();
 
 server.get("/", async (request, reply) => {
-  reply.code(200).send({ message: "Hello world!" });
+	reply.code(200).send({
+		backend: "active",
+		success: true,
+	});
+});
+
+// Scores Route
+
+server.get("/scores", async (request, reply) => {
+	reply.header("Access-Control-Allow-Origin", "*");
+	reply.code(200).send({
+		success: true,
+		data: {
+			robot: "Octave",
+			season: {
+				2021: {
+					scores: {
+						scrimmage: {
+							number: 1,
+							score: 6,
+						},
+						state: {
+							number: 1,
+							score: null,
+						},
+					},
+				},
+			},
+		},
+	});
 });
 
 server.listen(process.env.PORT || 8080, "0.0.0.0", (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+	console.log(`Server listening at ${address}`);
 });
